dae57fb6d3c6e4bea403e63b7c344b496a7bd651
coreytegeler <coreytegeler@gmail.com>
2018-07-16 10:42:42 -0400

    branched preview version

--- header.php
+++ header.php
@@ -9,6 +9,12 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 	<?php wp_head(); ?>
 	<title><?php bloginfo( 'name' ); ?></title>
+	<link rel="apple-touch-icon" sizes="180x180" href="<?= get_template_directory_uri(); ?>/assets/favicons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?= get_template_directory_uri(); ?>/assets/favicons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?= get_template_directory_uri(); ?>/assets/favicons/favicon-16x16.png">
+	<link rel="manifest" href="<?= get_template_directory_uri(); ?>/assets/favicons/site.webmanifest">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
 </head>
 
 <body <?php body_class(); ?>>
